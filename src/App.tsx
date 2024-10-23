887ff5d659ad2d9af9068f76591b286d563e6b9b
Miral Abbastada <156353012+MiralAbbastada@users.noreply.github.com>
2024-10-23 22:57:50 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Hamster from './icons/Hamster';
 import Heart from './images/ht.png';
-import { mainCharacter } from './images';
+import { binanceLogo, dollarCoin, mainCharacter } from './images';
 import Settings from './icons/Settings';
 import Friends from './icons/Friends';
 import Coins from './icons/Coins';
 import WebApp from '@twa-dev/sdk';
 
-// Интерфейс для данных пользователя
-interface UserData {
+interface UserData{
   id: number;
   first_name: string;
   last_name?: string;
@@ -19,61 +18,8 @@ interface UserData {
   is_premium?: boolean;
 }
 
-// API URL для работы с пользователями и монетами
-const API_URL = 'http://localhost:5000';
-
-// Получение или создание пользователя
-const getOrCreateUser = async (userData: UserData) => {
-  try {
-    const response = await fetch(`${API_URL}/start`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id: userData.id,
-        username: userData.username,
-        first_name: userData.first_name,
-        last_name: userData.last_name,
-        is_premium: userData.is_premium,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error('Ошибка при создании пользователя');
-    }
-
-    const data = await response.json();
-    return data.coins; // Возвращаем сохраненные монеты
-  } catch (error) {
-    console.error('Ошибка при создании пользователя:', error);
-    return null;
-  }
-};
-
-// Обновление монет пользователя
-const updateUserCoins = async (userId: number, coins: number) => {
-  try {
-    const response = await fetch(`${API_URL}/updateCoins`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id: userId,
-        coins: coins,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error('Ошибка при обновлении монет');
-    }
-  } catch (error) {
-    console.error('Ошибка при обновлении монет:', error);
-  }
-};
-
 const App: React.FC = () => {
+
   const levelNames = [
     "Bronze",    // From 0 to 4999 coins
     "Silver",    // From 5000 coins to 24,999 coins
@@ -100,35 +46,33 @@ const App: React.FC = () => {
     1000000000// DungeonMaster
   ];
 
-  const [levelIndex, setLevelIndex] = useState(0);
-  const [points, setPoints] = useState(0);
+  // Check if there are saved points and levelIndex in localStorage
+  const getInitialPoints = () => {
+    const savedPoints = localStorage.getItem('points');
+    return savedPoints ? parseInt(savedPoints) : 22749365; // Default value if no data is saved
+  };
+
+  const getInitialLevelIndex = () => {
+    const savedLevelIndex = localStorage.getItem('levelIndex');
+    return savedLevelIndex ? parseInt(savedLevelIndex) : 6; // Default value if no data is saved
+  };
+
+  const [levelIndex, setLevelIndex] = useState(getInitialLevelIndex());
+  const [points, setPoints] = useState(getInitialPoints());
   const [clicks, setClicks] = useState<{ id: number, x: number, y: number }[]>([]);
   const pointsToAdd = 5;
   const profitPerHour = 593043;
 
+  
   const [userData, setUserData] = useState<UserData | null>(null);
 
   useEffect(() => {
-    // Инициализация данных Telegram WebApp
-    if (WebApp.initDataUnsafe.user) {
-      const user = WebApp.initDataUnsafe.user as UserData;
-      setUserData(user);
-
-      // Получаем или создаем пользователя и устанавливаем его монеты
-      getOrCreateUser(user).then((savedCoins) => {
-        if (savedCoins !== null) {
-          setPoints(savedCoins);
-        }
-      });
+    if(WebApp.initDataUnsafe.user){
+      setUserData(WebApp.initDataUnsafe.user as UserData);
     }
-  }, []);
-
-  useEffect(() => {
-    if (userData) {
-      // Сохраняем обновленные монеты при изменении points
-      updateUserCoins(userData.id, points);
-    }
-  }, [points, userData]);
+    localStorage.setItem('points', points.toString());
+    localStorage.setItem('levelIndex', levelIndex.toString());
+  }, [points, levelIndex]);
 
   const handleCardClick = (e: React.MouseEvent<HTMLDivElement>) => {
     const card = e.currentTarget;
@@ -142,6 +86,7 @@ const App: React.FC = () => {
 
     setPoints(points + pointsToAdd);
     setClicks([...clicks, { id: Date.now(), x: e.pageX, y: e.pageY }]);
+
   };
 
   const handleAnimationEnd = (id: number) => {
@@ -192,13 +137,7 @@ const App: React.FC = () => {
               <Hamster size={24} className="text-[#d4d4d4]" />
             </div>
             <div>
-              {userData ? (
-                <p className="text-sm">
-                  {userData.first_name} {userData.last_name} (CEO)
-                </p>
-              ) : (
-                <p className="text-sm">Guest (CEO)</p>
-              )}
+              {userData ? (<p className="text-sm">{userData.first_name} {userData.last_name} (CEO)</p>) : <p className="text-sm">Guest (CEO)</p>}
             </div>
           </div>
           <div className="flex items-center justify-between space-x-4 mt-1">
@@ -206,9 +145,7 @@ const App: React.FC = () => {
               <div className="w-full">
                 <div className="flex justify-between">
                   <p className="text-sm">{levelNames[levelIndex]}</p>
-                  <p className="text-sm">
-                    {levelIndex + 1} <span className="text-[#95908a]">/ {levelNames.length}</span>
-                  </p>
+                  <p className="text-sm">{levelIndex + 1} <span className="text-[#95908a]">/ {levelNames.length}</span></p>
                 </div>
                 <div className="flex items-center mt-1 border-2 border-[#43433b] rounded-full">
                   <div className="w-full h-2 bg-[#43433b]/[0.6] rounded-full">
@@ -217,40 +154,71 @@ const App: React.FC = () => {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col items-center">
-              <img src={Heart} alt="Heart" className="h-6 w-6" />
-              <p className="text-sm mt-1">{formatProfitPerHour(profitPerHour)}</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <Coins size={24} className="text-[#fbbf24]" />
-              <p className="text-sm mt-1">{points}</p>
+            <div className="flex items-center w-2/3 border-2 border-[#43433b] rounded-full px-4 py-[2px] bg-[#43433b]/[0.6] max-w-64">
+              <div className="flex-1 text-center">
+                <p className="text-xs text-[#85827d] font-medium">Profit per hour</p>
+                <div className="flex items-center justify-center space-x-1">
+                  <img src={dollarCoin} alt="Dollar Coin" className="w-[18px] h-[18px]" />
+                  <p className="text-sm">{formatProfitPerHour(profitPerHour)}</p>
+                </div>
+              </div>
+              <div className="h-[32px] w-[2px] bg-[#43433b] mx-2"></div>
+              <Settings className="text-white" />
             </div>
           </div>
         </div>
-        <div className="mt-4 flex-1 flex items-center justify-center">
-          <div className="card h-48 w-48 bg-[#27292d]" onClick={handleCardClick}>
-            <img src={mainCharacter} alt="Character" className="h-full w-full" />
+
+        <div className="absolute top-[25%] left-0 right-0 bottom-0 rounded-t-[55px]">
+
+            <div className="px-4 mt-4 flex justify-center">
+              <div className="px-4 py-2 flex items-center space-x-2">
+                <img src={dollarCoin} alt="Dollar Coin" className="w-10 h-10" />
+                <p className="text-4xl font-weight-bolder text-white">{points.toLocaleString()}</p>
+              </div>
+            </div>
+
+            <div className="px-4 mt-4 flex justify-center">
+              <div
+                className="w-90 h-90 p-4 rounded-full circle-outer"
+                onClick={handleCardClick}
+              >
+                <div className="w-full p-10 h-full rounded-full circle-inner">
+                  <img src={mainCharacter} alt="Main Character" className="mx-auto m-10" />
+                </div>
+              </div>
+            </div>
           </div>
+      </div>
+
+      {/* Bottom fixed div */}
+      <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-gradient-to-r from-[#f5f5f5]/10 to-[#FFFFFF]/5 mb-5 flex justify-around items-center p-5 z-50 rounded-[5000px] text-xs">
+        <div className="text-center text-yellow w-auto bg-[#1C1B18] m-1 p-2 rounded-[5000px]">
+          <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" />
         </div>
-        <div className="absolute w-full h-full top-0 left-0 z-0">
-          {clicks.map((click) => (
-            <div
-              key={click.id}
-              className="absolute w-10 h-10 bg-white/20 rounded-full animate-ping"
-              style={{ left: click.x - 20, top: click.y - 20 }}
-              onAnimationEnd={() => handleAnimationEnd(click.id)}
-            ></div>
-          ))}
+        <div className="text-center text-[#85827d] w-1/5">
+          <Friends className="w-8 h-8 mx-auto" />
+          <p className="mt-1">Friends</p>
         </div>
-        <div className="w-full flex justify-around py-4">
-          <button>
-            <Friends size={24} className="text-[#d4d4d4]" />
-          </button>
-          <button>
-            <Settings size={24} className="text-[#d4d4d4]" />
-          </button>
+        <div className="text-center text-[#85827d] w-1/5">
+          <Coins className="w-8 h-8 mx-auto" />
+          <p className="mt-1">Earn</p>
         </div>
       </div>
+
+      {clicks.map((click) => (
+        <div
+          key={click.id}
+          className="absolute text-5xl font-bold opacity-0 text-white pointer-events-none"
+          style={{
+            top: `${click.y - 42}px`,
+            left: `${click.x - 28}px`,
+            animation: `float 1s ease-out`
+          }}
+          onAnimationEnd={() => handleAnimationEnd(click.id)}
+        >
+          <img src={Heart} alt="" />
+        </div>
+      ))}
     </div>
   );
 };
